154324e5ed2c3d54507ebc3de3747b3062e69c3b
vinoven <mikhailsechin@gmail.com>
2020-11-11 02:06:47 +0300

    Удаляет неиспользующийся код из модуля util

--- js/util.js
+++ js/util.js
@@ -1,26 +1,6 @@
 'use strict';
 
 (() => {
-  const getRandomItemFromArray = (array) => {
-    const arrayItem = array[Math.floor(Math.random() * array.length)];
-    return arrayItem;
-  };
-
-  const getMultipleRandomItemsFromArray = (array) => {
-    let generatedArray = [];
-    for (let i = 0; i < array.length; i++) {
-      let checker = Math.round(Math.random());
-      if (checker) {
-        generatedArray.push(array[i]);
-      }
-    }
-    return generatedArray;
-  };
-
-  const getRandomIntegerFromRange = (min, max) => {
-    return Math.round(Math.random() * (max - min) + min);
-  };
-
   const removeElements = (elements) => {
     while (elements.length > 0) {
       elements[0].remove();
@@ -34,9 +14,6 @@
   };
 
   window.util = {
-    'getRandomItemFromArray': getRandomItemFromArray,
-    'getMultipleRandomItemsFromArray': getMultipleRandomItemsFromArray,
-    'getRandomIntegerFromRange': getRandomIntegerFromRange,
     'removeElements': removeElements,
     'toggleElementsState': toggleElementsState
   };
